194cc27c221286e27ec1f11636acd6b5e5117dbf
Otoniel Perez <otonielcuentas@gmail.com>
2021-04-13 11:32:46 +0100

    Commit

---
+++ run.py
@@ -0,0 +1,84 @@
+# Search methods
+
+import search
+
+ab = search.GPSProblem('A', 'B'
+                       , search.romania)
+ah = search.GPSProblem('A', 'H'
+                       , search.romania)
+
+node, count = search.breadth_first_graph_search(ab)
+print('Busqueda por BFS')
+print(node.path(), end='')
+print('\nNodos visitados: ' + str(count) + '\n')
+
+node, count = search.depth_first_graph_search(ab)
+print('Busqueda por DFS')
+print(node.path(), end='')
+print('\nNodos visitados: ' + str(count) + '\n')
+
+# Parte 1
+node, count = search.branch_and_bound_search(ab)
+print('Busqueda por Branch and Bound')
+print(node.path(), end='')
+print('\nNodos visitados: ' + str(count) + '\n')
+
+# Parte 2
+node, count = search.branch_and_bound_SubEstimation_search(ab)
+print('Busqueda por Branch and Bound subestimada (heurística)')
+print(node.path(), end='')
+print('\nNodos visitados: ' + str(count) + '\n')
+
+# Result:
+# BFS
+# [<Node B>, <Node F>, <Node S>, <Node A>] : 211 + 99 + 140 = 450
+# DFS
+# [<Node B>, <Node P>, <Node C>, <Node D>, <Node M>, <Node L>, <Node T>, <Node A>] : 101 + 138 + 120 + 75 + 70 + 111 + 118 = 733
+# BNB
+# [<Node B>, <Node P>, <Node R>, <Node S>, <Node A>] : 101 + 97 + 80 + 140 = 418
+
+## PRUEBAS PROPIAS
+
+print('PRUEBA 2: O a E')
+
+oe = search.GPSProblem('O', 'E'
+                       , search.romania)
+
+node, count = search.depth_first_graph_search(oe)
+print('Busqueda por DFS')
+print(node.path(), end='')
+print('\nNodos visitados: ' + str(count) + '\n')
+
+# Parte 1
+node, count = search.branch_and_bound_search(oe)
+print('Busqueda por Branch and Bound')
+print(node.path(), end='')
+print('\nNodos visitados: ' + str(count) + '\n')
+
+# Parte 2
+node, count = search.branch_and_bound_SubEstimation_search(oe)
+print('Busqueda por Branch and Bound subestimada (heurística)')
+print(node.path(), end='')
+print('\nNodos visitados: ' + str(count) + '\n')
+
+print('PRUEBA 3: Z a C')
+
+zc = search.GPSProblem('Z', 'C'
+                       , search.romania)
+
+node, count = search.depth_first_graph_search(zc)
+print('Busqueda por DFS')
+print(node.path(), end='')
+print('\nNodos visitados: ' + str(count) + '\n')
+
+# Parte 1
+node, count = search.branch_and_bound_search(zc)
+print('Busqueda por Branch and Bound')
+print(node.path(), end='')
+print('\nNodos visitados: ' + str(count) + '\n')
+
+# Parte 2
+node, count = search.branch_and_bound_SubEstimation_search(zc)
+print('Busqueda por Branch and Bound subestimada (heurística)')
+print(node.path(), end='')
+print('\nNodos visitados: ' + str(count) + '\n')
